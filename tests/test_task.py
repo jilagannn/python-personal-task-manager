22b97974ecd4798165ffab1e151dcee837b4a5de
Jheyrus Ilagan <jllagan@academic.rrc.ca>
2025-08-14 02:26:59 -0500

    Defined test cases for 'Task Manager' and 'Task' classes.

---
+++ tests/test_task.py
@@ -0,0 +1,14 @@
+"""This modules defines the test cases for the Task class.
+
+Test Cases:
+"""
+
+import unittest
+from unittest import TestCase
+from task.task import Task
+
+__author__ = "Jheyrus Ilagan"
+__version__ = "10.14.2025"
+
+class TestTask(TestCase):
+    """Defines the unit tests for Task class."""
---
+++ tests/test_task_manager.py
@@ -0,0 +1,12 @@
+"""This modules defines the test cases for the Task Manager class.
+
+Test Cases:
+"""
+
+import unittest
+from unittest import TestCase
+from task.task import Task
+from manager.task_manager import TaskManager
+
+__author__ = "Jheyrus Ilagan"
+__version__ = "10.14.2025"
