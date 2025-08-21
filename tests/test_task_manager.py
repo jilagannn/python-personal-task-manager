ba81a60c9e70616e3393dad6b603e547bc7bf544
Jheyrus Ilagan <jllagan@academic.rrc.ca>
2025-08-21 16:25:36 -0500

    Defined test case for 'add_task()' method ensuring task content is preserved when adding tasks with regular strings.

--- tests/test_task_manager.py
+++ tests/test_task_manager.py
@@ -17,8 +17,9 @@ Test Cases:
     - Saved tasks contain correct data after saving.
 
     add_task()
-    - Tasks are successfully added as task objects.
+    - Verifies the correct number of task quantity added as task objects.
     - Tasks are successfully added when empty values are given.
+    - Tasks are successfully added when regular values are given.
 """
 
 import unittest
@@ -264,7 +265,7 @@ class TestTaskManager(TestCase):
         self.assertEqual(expected_task_status, actual_task_status)
 
     ### add_task()
-    def test_add_task_successfully_adds_task_object(self):
+    def test_add_task_successfully_adds__correct_quantity_of_task_objects(self):
         # Arrange
         file_name = "add-task-test-list.json"
         if os.path.exists(file_name):
@@ -284,6 +285,41 @@ class TestTaskManager(TestCase):
         # Assert
         expected_number_of_tasks = 1
         self.assertEqual(expected_number_of_tasks, actual_number_of_tasks)
+
+    def test_add_task_successfully_adds_task_object_with_regular_strings(self):
+        # Arrange
+        file_name = "add-task-test-list.json"
+        if os.path.exists(file_name):
+            os.remove(file_name)
+        task_manager = TaskManager(file_name)
+
+        # Act
+        task_components = ["Test Task 1", "Test task to be added.", "08-30-2025", "Medium"]
+
+        # Capturing the console printed message to keep test clean.
+        with patch("builtins.input", side_effect=task_components), \
+        patch("sys.stdout", new=StringIO()): 
+            task_manager.add_task() 
+        actual_tasks = task_manager.tasks[0]
+        actual_task_name = actual_tasks.task_name
+        actual_task_description = actual_tasks.task_description
+        actual_task_due_date = actual_tasks.date_due
+        actual_task_priority = actual_tasks.priority
+        actual_task_status = actual_tasks.completed
+        actual_number_of_tasks = len(task_manager.tasks)
+        # Assert
+        expected_task_name = "Test Task 1"
+        expected_task_description = "Test task to be added."
+        expected_task_due_date = "08-30-2025"
+        expected_task_priority = "Medium"
+        expected_task_status = False
+        expected_number_of_tasks = 1
+        self.assertEqual(expected_number_of_tasks, actual_number_of_tasks)
+        self.assertEqual(expected_task_name, actual_task_name)
+        self.assertEqual(expected_task_description, actual_task_description)
+        self.assertEqual(expected_task_due_date, actual_task_due_date)
+        self.assertEqual(expected_task_priority,actual_task_priority)
+        self.assertEqual(expected_task_status, actual_task_status)
     
     def test_add_task_successfully_adds_task_object_with_empty_strings(self):
         # Arrange
