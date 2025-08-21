26ed0b1e3513891da9203b590e7ba932bc901fdb
Jheyrus Ilagan <jllagan@academic.rrc.ca>
2025-08-20 20:31:36 -0500

    Defined test case for 'add_task()' method ensuring tasks are successfully added when given empty values.

--- tests/test_task_manager.py
+++ tests/test_task_manager.py
@@ -18,6 +18,7 @@ Test Cases:
 
     add_task()
     - Tasks are successfully added as task objects.
+    - Tasks are successfully added when empty values are given.
 """
 
 import unittest
@@ -270,7 +271,6 @@ class TestTaskManager(TestCase):
             os.remove(file_name)
         task_manager = TaskManager(file_name)
 
-
         # Act
         task_components = ["Test Task 1", "Test task to be added.", "08-30-2025", "Medium"]
 
@@ -284,3 +284,39 @@ class TestTaskManager(TestCase):
         # Assert
         expected_number_of_tasks = 1
         self.assertEqual(expected_number_of_tasks, actual_number_of_tasks)
+    
+    def test_add_task_successfully_adds_task_object_with_empty_strings(self):
+        # Arrange
+        file_name = "add-task-test-list-2.json"
+        if os.path.exists(file_name):
+            os.remove(file_name)
+        task_manager = TaskManager(file_name)
+
+        # Act
+        task_components = ["", "", "", ""]
+
+        # Capturing the console printed message to keep test clean.
+        with patch("builtins.input", side_effect=task_components), \
+        patch("sys.stdout", new=StringIO()): 
+            task_manager.add_task() 
+        actual_tasks = task_manager.tasks[0]
+        actual_task_name = actual_tasks.task_name
+        actual_task_description = actual_tasks.task_description
+        actual_task_due_date = actual_tasks.date_due
+        actual_task_priority = actual_tasks.priority
+        actual_task_status = actual_tasks.completed
+        actual_number_of_tasks = len(task_manager.tasks)
+        # Assert
+        expected_task_name = "Test Task"
+        expected_task_description = ""
+        expected_task_due_date = ""
+        expected_task_priority = "Medium"
+        expected_task_status = False
+        expected_number_of_tasks = 1
+        self.assertEqual(expected_number_of_tasks, actual_number_of_tasks)
+        self.assertEqual(expected_task_name, actual_task_name)
+        self.assertEqual(expected_task_description, actual_task_description)
+        self.assertEqual(expected_task_due_date, actual_task_due_date)
+        self.assertEqual(expected_task_priority,actual_task_priority)
+        self.assertEqual(expected_task_status, actual_task_status)
+        
