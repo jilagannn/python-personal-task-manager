5446a7a08db5430d47509d2ba0c55975d95427b1
Jheyrus Ilagan <jllagan@academic.rrc.ca>
2025-09-07 02:14:37 -0500

    refactor: Refactored entire update_task() method into smaller methods for better readability and testing purposes.

--- manager/task_manager.py
+++ manager/task_manager.py
@@ -1,18 +1,18 @@
 """This module contains the Task Manager Class."""
 
+__author__ = "Jheyrus Ilagan"
+__version__ = "05.19.2025"
+
 import os
 import json
 from task.task import Task
-
-__author__ = "Jheyrus Ilagan"
-__version__ = "05.19.2025"
 
 class TaskManager:
     """Class representing the main Task Manager program."""
 
     def __init__(self, filename="task-list.json"):
         """Initializes the personal Task Manager.
-        
+
         Args:
             filename (str): The filename that the tasks will be written
             out to. Default string value will be "task-list.json
@@ -24,10 +24,11 @@ class TaskManager:
 
     def view_task(self) -> None:
         """Views the list of tasks from the specified file."""
-        border = "=" * 40
+
+        border = "=" * 80
         if self.tasks:
             task_list_title = "My Tasks"
-            print(f"{border}\n{task_list_title:^40}\n{border}")
+            print(f"{border}\n{task_list_title:^80}\n{border}")
             for index, task in enumerate(self.tasks, 1):
                 print(f"\n{index}. {task}\n")
             print(border)
@@ -59,12 +60,11 @@ class TaskManager:
                 saved_data.append(task_dictionary)
             json.dump(saved_data, file, indent=2)
 
-    
     def add_task(self) -> None:
          """Adds tasks to the task list."""
-         border = "=" * 40
+         border = "=" * 80
          add_task_message = "Add Task"
-         print(f"{border}\n{add_task_message:^40}\n{border}")
+         print(f"{border}\n{add_task_message:^80}\n{border}")
          task_name = input("Enter the name of your task: ")
          task_description = input("Brief description of your task: ")
          due_date = input("Enter the due date (DD-MM-YYYY): ")
@@ -72,72 +72,122 @@ class TaskManager:
 
          user_task = Task(task_name, task_description, due_date, priority_level)
          self.tasks.append(user_task)
-         print("=" * 40 + "\nTask has been added successfully! \nReturning to menu select.")
+         print("=" * 80 + "\nTask has been added successfully! \nReturning to menu select.")
          self.save_task()
+
+    def display_task_list(self, title="Tasks") -> None:
+        """Displays the current task list for the user."""
+
+        border = "=" * 80
+        print(f"{border}\n{title:^80}\n{border}")
+        for index, task in enumerate(self.tasks, 1):
+            print(f"\n{index}. {task}\n")
+        print(border)
 
     def update_task(self) -> None:
         """Updates the selected task from existing task list."""
 
-        if self.tasks:
-            border = "=" * 40
-            update_task_title = "Update Task"
-            component_title = "Update Task Components"
-            name_option = "1. Name"
-            description_option = "2. Description"
-            due_date_option = "3. Due Date"
-            priority_level_option = "4. Priority Level"
-            status_option = "5. Status"
-            menu_exit = "6. Exit"
-            print(f"{border}\n{update_task_title:^40}\n{border}")
-            for index, task in enumerate(self.tasks, 1):
-                print(f"\n{index}. {task}\n")
-            print(border)
-            user_input = int(input("Enter the task " \
-            "# you would like to update: "))
-            print(border)
-            task_list = user_input - 1
-            desired_task = self.tasks[task_list]
-            print (desired_task)
-            menu_selection = ""
-            while menu_selection != 6:
-                print(f"{border}\n{component_title:^40}\n\n{name_option}\n{description_option}\n{due_date_option}"
-                      f"\n{priority_level_option}\n{status_option}\n{menu_exit}\n")
-                menu_selection = int(input("Which field of the task did you want to update?: "))
-                if menu_selection == 1:
-                    updated_name = input(f"{border}\nEnter the desired task name: ")
-                    desired_task.task_name = updated_name
-                    print(f"{border}\nTask name has been updated.")
-                elif menu_selection == 2:
-                    updated_description = input(f"{border}\nEnter the new description for your " \
-                    "task: ")
-                    desired_task.task_description = updated_description
-                    print(f"{border}\nTask description has been updated.")
-                elif menu_selection == 3:
-                    updated_due_date = input(f"{border}\nEnter the new due date for your task "
-                    "in the following format DD-MM-YYYY: ")
-                    desired_task.date_due = updated_due_date
-                    print(f"{border}\nTask due date has been updated.")
-                elif menu_selection == 4:
-                    updated_priority_level = input(f"{border}\nDetermine the new priority level " \
-                    "for your task: ").upper()
-                    desired_task.priority = updated_priority_level
-                    print(f"{border}\nTask priority level has been updated.")
-                elif menu_selection == 5:
-                    updated_status = input(f"{border}\nIs your task completed? " \
-                    "\nEnter y/n: ").lower()
-                    if updated_status == "y":
-                        updated_status = True
-                        print(f"{border}\nTask has been marked as complete.")
-                    else:
-                        updated_status = False
-                    desired_task.completed = updated_status
-                
-            else:
-                self.save_task()
-                print("Exiting task update menu.")
-        
+        if not (self.tasks):
+             print("No tasks to update. Please add a task first!")
+
         else:
-            print("No tasks to update. Please add a task first!")
+             selected_task = self.select_task_to_update()
+             print(selected_task)
+             self.update_task_field(selected_task)
+             self.save_task()
+             print("Exiting task update menu.")
+
+    def select_task_to_update(self) -> Task:
+        """Acquires the selected task to be updated.
+        
+        Returns:
+            Task: Task object selected from the list.
+        """
+        
+        self.display_task_list(title="Update Task")
+        user_input = int(input("Enter the task # you would like to update: "))
+        print("=" * 80)
+        task_list = user_input - 1
+        desired_task = self.tasks[task_list]
+        
+        return desired_task
+    
+    def display_and_select_update_menu_option(self) -> int:
+        """Displays the task components available for update.
+        
+        Returns:
+            int: The field of the task to be updated.
+        """
+
+        border = "=" *80
+        component_title = "Update Task Components"
+        name_option = "1. Name"
+        description_option = "2. Description"
+        due_date_option = "3. Due Date"
+        priority_level_option = "4. Priority Level"
+        status_option = "5. Status"
+        menu_exit = "6. Exit"
+
+        print(f"{border}\n{component_title:^80}\n\n{name_option}\n{description_option}\n{due_date_option}"
+                    f"\n{priority_level_option}\n{status_option}\n{menu_exit}\n")
+        selected_field = int(input("Which field of the task did you want to update?: "))
+
+        return selected_field
+    
+    def update_single_field_of_task(self, task, field) -> None:
+        """Updates a single field of a task."""
+
+        border = "=" * 80
+        if field == 1:
+                    updated_name = input(f"{border}\nEnter the desired task name: ")
+                    task.task_name = updated_name
+                    print(f"{border}\nTask name has been updated.")
+
+        elif field == 2:
+            updated_description = input(f"{border}\nEnter the new description for your " \
+            "task: ")
+            task.task_description = updated_description
+            print(f"{border}\nTask description has been updated.")
+
+        elif field == 3:
+            updated_due_date = input(f"{border}\nEnter the new due date for your task "
+            "in the following format DD-MM-YYYY: ")
+            task.date_due = updated_due_date
+            print(f"{border}\nTask due date has been updated.")
+
+        elif field == 4:
+            updated_priority_level = input(f"{border}\nDetermine the new priority level " \
+            "for your task: ").upper().strip()
+
+            if updated_priority_level == "":
+                 task.priority = "Medium"
+                 print(f"{border}\nTask priority level has been updated.")
+
+            else:
+                task.priority = updated_priority_level
+                print(f"{border}\nTask priority level has been updated.")
+
+        elif field == 5:
+            updated_status = input(f"{border}\nIs your task completed? " \
+            "\nEnter y/n: ").lower()
+            
+            if updated_status == "y":
+                updated_status = True
+                print(f"{border}\nTask has been marked as complete.")
+            
+            else:
+                updated_status = False
+
+            task.completed = updated_status
+
+    def update_task_field(self, task) -> None:
+        """Updates the selected field of the task."""
+
+        menu_selection = ""
+        while menu_selection != 6:
+            menu_selection =  self.display_and_select_update_menu_option()
+            if menu_selection in range(1, 6):
+                 self.update_single_field_of_task(task, menu_selection)
 
     def delete_task(self):
         """Deletes a task from the task list."""
@@ -146,7 +196,7 @@ class TaskManager:
             print("There are no tasks to delete.")
         else:  
             self.tasks
-            border = "=" * 40
+            border = "=" * 80
             delete_title = "Delete Task(s)"
             finished_tasks = "1. Completed Tasks"
             singular_task = "2. Single Task From List"
@@ -155,7 +205,7 @@ class TaskManager:
             task_list_title = "My Tasks"
             category_selection = 1111
             while category_selection != 4:
-                category_selection = int(input(f"{border}\n{delete_title:^40}\n{border}\n"
+                category_selection = int(input(f"{border}\n{delete_title:^80}\n{border}\n"
                                                f"\n{finished_tasks}\n{singular_task}\n{all_tasks}\n"
                                                f"{return_to_main_menu}\n\n{border}"
                                                f"\nSelect a category by inputting the "
@@ -262,14 +312,14 @@ class TaskManager:
          the program.
          """
          start_message = "Personal Task Manager"
-         border = "=" * 40
+         border = "=" * 80
          view_task = "1. View Task"
          add_task = "2. Add Task"
          update_task = "3. Update Task"
          delete_task = "4. Delete Task"
          exit_system = "5. Exit"
 
-         print(f"{border}\n{start_message:^40}\n\n{view_task}\n{add_task}\n"
+         print(f"{border}\n{start_message:^80}\n\n{view_task}\n{add_task}\n"
                f"{update_task}\n{delete_task}\n{exit_system}\n\n{border}")
          
          return input("Select an option (1-5): ")
